8e9e6d9ca4ec7ce6b705fb29c847ecc61008f5c6
Dankimhaejun <dan.kimhaejun@gmail.com>
2024-02-22 11:42:18 +0900

    feat: Create common 404 error component

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+import NotFound from '@/components/common/not-found';
+import {NextPage} from 'next';
+
+const NotFoundPage: NextPage = () => {
+  return <NotFound />;
+};
+
+export default NotFoundPage;
